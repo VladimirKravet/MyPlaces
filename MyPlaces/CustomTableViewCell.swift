d37be388f179658ebc7d938d4bdd0705aecc53a4
Vladimir Kravets <natccorp@gmail.com>
2022-10-21 17:46:43 +0300

    Custom Table View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Vladimir Kravets on 21.10.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
